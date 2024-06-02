6cd07dbae01fef4ff8adf2102d072ce28825d0bc
Rui Lucas <lucasantony815@gmail.com>
2024-06-02 15:16:57 -0300

    debug app

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export function App() {
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      { !!user ? <SendMessageForm /> : <LoginBox /> }
+      { user ? <SendMessageForm /> : <LoginBox /> }
     </main>
   )
 }
